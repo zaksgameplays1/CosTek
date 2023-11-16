147a7dd68d7f240cf04f6b331d5af9e4965a6718
zaksgameplays1 <elfandotheranimatedthing@gmail.com>
2023-11-16 23:41:45 +0000

    a

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 // main.js
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+    mainWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
         webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true,
-            contextIsolation: false,
-        },
-    });
-
-    mainWindow.loadFile('index.html');
-}
-
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
+            nodeIntegration: true
         }
     });
-});
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
     }
 });
